63dc2c5c0e74fe459995859a006ee869e3aebbbc
candra06 <candrabiyu4@gmail.com>
2022-01-24 14:42:23 +0700

    add foto in login response

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -64,7 +64,7 @@ class AuthController extends Controller
                         $data['nama'] = $guru->nama;
                         $data['alamat'] = $guru->alamat;
                         $data['id_users'] = $guru->id_users;
-                        $data['phone'] = $guru->phone;
+                        $data['phone'] = $user->phone;
                         $data['foto'] = $user->foto;
                         $data['role'] = $user->role;
                         $data['username'] = $user->username;
