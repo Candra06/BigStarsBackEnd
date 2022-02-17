9306ff0f2b3f7c19a6c66e5658aa4c5963283752
candra06 <candrabiyu4@gmail.com>
2022-02-17 22:26:50 +0700

    report filter

--- app/Http/Controllers/API/PembayaranFeeController.php
+++ app/Http/Controllers/API/PembayaranFeeController.php
@@ -218,7 +218,7 @@ class PembayaranFeeController extends Controller
                 'data' => $dt
             ]);
         } catch (\Throwable $th) {
-            //throw $th;
+            throw $th;
         }
     }
 
