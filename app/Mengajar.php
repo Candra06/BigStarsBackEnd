3779cca5fbfb86f241fc56347cefd7d67214eb37
candra06 <candrabiyu4@gmail.com>
2022-03-03 23:24:19 +0700

    kehadirn by admin

--- app/Mengajar.php
+++ app/Mengajar.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Mengajar extends Model
 {
     protected $table = 'mengajar';
-    protected $fillable = ['id_guru', 'id_kelas', 'tipe', 'spp', 'fee_pengajar', 'poin_siswa', 'status', 'file_materi', 'jurnal', 'latitude','longitude','materi'];
+    protected $fillable = ['id_guru', 'id_kelas', 'tipe', 'spp', 'fee_pengajar', 'poin_siswa', 'status', 'file_materi', 'jurnal', 'latitude','longitude','materi', 'created_at', 'updated_at'];
 }
